daa8e24ad43dc44ffc1ae35d477ee85f2db233c3
kjorda02 <krishnajordajimenez@gmail.com>
2022-11-24 13:30:11 +0100

    Arreglado mensajes de debug

--- Practica2_Minishell/nivel1.c
+++ Practica2_Minishell/nivel1.c
@@ -136,7 +136,7 @@ int parse_args(char **args, char *line) {
     // si args[i]!= NULL && *args[i]!='#' pasamos al siguiente token
     while (args[i] && args[i][0] != '#') {
         #if DEBUGN1
-            printf(GRIS_T"parse_args()→token %d: %s\n"RESET_FORMATO, i, args[i]);  // Mensaje de debug
+            fprintf(stderr, GRIS_T"parse_args()→token %d: %s\n"RESET_FORMATO, i, args[i]);  // Mensaje de debug
         #endif
         i++;
         args[i] = strtok(NULL, " \t\n\r");
@@ -183,37 +183,43 @@ int check_internal(char **args) {
 
 
 int internal_cd(char **args) {
-    printf(GRIS_T"[internal_cd()→ comando interno no implementado]\n"RESET_FORMATO);
+    #if DEBUGN1 
+        fprintf(stderr, GRIS_T"[internal_cd()→ comando interno no implementado]\n"RESET_FORMATO);
+    #endif
     return 1;
 } 
 
 int internal_export(char **args) {
-    printf(GRIS_T"[internal_export()→ comando interno no implementado]\n"RESET_FORMATO);
+    #if DEBUGN1 
+        fprintf(stderr, GRIS_T"[internal_export()→ comando interno no implementado]\n"RESET_FORMATO);
+    #endif
     return 1;
 }
 
 int internal_source(char **args) {
-    printf(GRIS_T"[internal_source()→ comando interno no implementado]\n"RESET_FORMATO);
+    #if DEBUGN1 
+        fprintf(stderr, GRIS_T"[internal_source()→ comando interno no implementado]\n"RESET_FORMATO);
+    #endif
     return 1;
 }
 
 int internal_jobs(char **args) {
     #if DEBUGN1 
-        printf(GRIS_T"[internal_jobs()→ Esta función mostrará el PID de los procesos que no estén en foreground]\n"RESET_FORMATO);
+        fprintf(stderr, GRIS_T"[internal_jobs()→ Esta función mostrará el PID de los procesos que no estén en foreground]\n"RESET_FORMATO);
     #endif
     return 1;
 }
 
 int internal_fg(char **args) {
     #if DEBUGN1 
-        printf(GRIS_T"[internal_fg()→ Esta función enviará un trabajo detenido al foreground reactivando su ejecución, o uno del background al foreground ]\n"RESET_FORMATO);
+        fprintf(stderr, GRIS_T"[internal_fg()→ Esta función enviará un trabajo detenido al foreground reactivando su ejecución, o uno del background al foreground ]\n"RESET_FORMATO);
     #endif
     return 1;
 }
 
 int internal_bg(char **args) {
     #if DEBUGN1 
-        printf(GRIS_T"[internal_bg()→ Esta función reactivará un proceso detenido para que siga ejecutándose pero en segundo plano]\n"RESET_FORMATO);
+        fprintf(stderr, GRIS_T"[internal_bg()→ Esta función reactivará un proceso detenido para que siga ejecutándose pero en segundo plano]\n"RESET_FORMATO);
     #endif
     return 1;
 }
