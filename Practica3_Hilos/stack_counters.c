041323298e4cca803746a04bce4b4af59674678f
Valentino Coppola Ferrari <116271468+ValenCF@users.noreply.github.com>
2023-01-09 21:53:39 +0100

    Update stack_counters.c

--- Practica3_Hilos/stack_counters.c
+++ Practica3_Hilos/stack_counters.c
@@ -11,53 +11,55 @@ struct my_stack* pila;
 int OriginalLenght=0;
 
 int main (int argc, char *argv[]){
+    //si se ha pasado el nombre del fichero por consola lo guardamos en 
     if (argc > 1){
         strcpy(nombreFichero, argv[1]);
     }
+    //ERROR
     else{
         fprintf(stderr, "Error, sintaxis: ./stack_counters <nombre_fichero_pila>\n");
     }
 
     // Verificar si la pila existe
-    pila = my_stack_read(nombreFichero);
+    pila = my_stack_read(argv[1]);
+    //si la pila no existe la creamos
     if (!pila){
         pila = my_stack_init(sizeof(int));
-    }
-    else{
-         ///if(my_stack_len(pila)<10){
-            //Mientras haya menos de 10 nodos
-            while(my_stack_len(pila)<NUM_THREADS){
-            //creamos nuevo nodo en la pila
-                my_stack_push(pila,my_stack_len(pila));
-            }
-       // }
     }
     exit(0);
 
     pthread_t identificadores[NUM_THREADS];  
+    //realizamos un bucle para crear los hilos
     for (int i = 0; i < NUM_THREADS; i++){
         pthread_create(&identificadores[i], NULL, worker, NULL);
     }
-        printf("\nThreads: %d, Itreations: %d \n", NUM_THREADS, N_ITERACIONES);
-        printf("stack->size: %d \n", pila->size);
-        printf("original stack lenght: %d \n", OriginalLenght);
-        printf("new stack lenght: %d \n\n", my_stack_len(pila));
 
+    printf("\nThreads: %d, Itreations: %d \n", NUM_THREADS, N_ITERACIONES);
+    printf("stack->size: %d \n", pila->size);
+    printf("original stack lenght: %d \n", OriginalLenght);
+    printf("new stack lenght: %d \n\n", my_stack_len(pila));
+
+        //bloqueamos el hilo principal
         pthread_join(identificadores[0],NULL);
-        my_stack_write(pila,nombreFichero);
+        //guardamos la pila en el fichero
+        my_stack_write(pila,argv[1]);
         pthread_exit(NULL);
 }
 
 void *worker(void *ptr){
 
     for (int i = 0; i < N_ITERACIONES; i++){
-
+        //cogo el primer valor y pongo en espera al reesto de procesos
+        //para evitar que accedan a la pila
         pthread_mutex_lock(&semaforo);
-        void *data = my_stack_pop(pila);
+        //saco el valor de la pila y lo incremento
+        int *data = my_stack_pop(pila);
         *data=*data+1;
-        my_stack_push(pila,data)
+        //introduzco el valor ya incrementado
+        my_stack_push(pila,data);
+        //desbloqueo la pila
         pthread_mutex_unlock(&semaforo);
-        pthread_exit(NULL);
     }
+       pthread_exit(NULL);
     
 }
